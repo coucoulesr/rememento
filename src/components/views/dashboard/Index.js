0649bde4778a871f8a131ad06d3465581fabe0a5
Richard Coucoules <coules93@gmail.com>
2020-05-02 19:48:10 -0700

    Add logic if token is present but not other props

--- src/components/views/dashboard/Index.js
+++ src/components/views/dashboard/Index.js
@@ -2,20 +2,48 @@ import React from "react";
 import Nav from "./Nav";
 import RecipientList from "./RecipientList";
 import Settings from "./Settings";
-import { connect } from "react-redux";
 import { changeRecipients } from "../../../redux/actions";
+import axios from "axios";
+import { connect } from "react-redux";
+import { login } from "../../../redux/actions";
+import { CircularProgress } from "@material-ui/core";
 
-const mapStateToProps = (state) => ({
-  name: state.name,
-  email: state.email,
-  token: state.token,
-  recipients: state.recipients,
-});
+const mapStateToProps = (state) => {
+  return {
+    email: state.email,
+    name: state.name,
+    recipients: state.recipients,
+    token: state.token,
+  };
+};
 
 class Dashboard extends React.Component {
   state = {
     viewSettings: false,
+    readyToRender: false,
   };
+
+  componentWillMount() {
+    if (!this.props.email) {
+      axios({
+        method: "GET",
+        url: "https://api.racoucoules.com/rememento/fetch-user",
+        headers: { Authorization: this.props.token },
+      }).then((res) => {
+        this.props.dispatch(
+          login(
+            res.data.email,
+            res.data.name,
+            res.data.recipients,
+            this.props.token
+          )
+        );
+        this.setState({ ...this.state, readyToRender: true });
+      });
+    } else if (this.props.name && this.state.recipients) {
+      this.setState({ ...this.state, readyToRender: true });
+    }
+  }
 
   toggleSettings = () => {
     this.setState({ ...this.state, viewSettings: !this.state.viewSettings });
@@ -24,19 +52,22 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div style={bodyStyles}>
-        <Nav
-          name={this.props.name}
-          toggleSettings={this.toggleSettings}
-          viewSettings={this.state.viewSettings}
-        />
-        {!this.state.viewSettings && (
+        {!this.state.readyToRender && <CircularProgress />}
+        {this.state.readyToRender && (
+          <Nav
+            name={this.state.readyToRender && this.props.name}
+            toggleSettings={this.toggleSettings}
+            viewSettings={this.state.viewSettings}
+          />
+        )}
+        {!this.state.viewSettings && this.state.readyToRender && (
           <RecipientList
             changeRecipients={(r) => this.props.dispatch(changeRecipients(r))}
             token={this.props.token}
             recipients={this.props.recipients}
           />
         )}
-        {this.state.viewSettings && (
+        {this.state.viewSettings && this.state.readyToRender && (
           <Settings
             recipients={this.props.recipients}
             token={this.props.token}
