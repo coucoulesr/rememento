815cc121134450072786719f4524bd66f280f73b
Richard Coucoules <coules93@gmail.com>
2020-05-03 09:11:49 -0700

    Add header to recipient cards

--- src/components/views/dashboard/RecipientList.js
+++ src/components/views/dashboard/RecipientList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Recipient from "./Recipient";
 import cardStyles from "./styles/card-styles";
+import { Typography } from "@material-ui/core";
 
 class RecipientList extends React.Component {
   changeRecipients = (r) => {
-    const recipients = this.props.recipients
+    const recipients = this.props.recipients;
     this.props.changeRecipients(
       recipients.map((recipient) => {
         if (recipient.id == r.id) {
@@ -20,6 +21,7 @@ class RecipientList extends React.Component {
     return this.props.recipients.map((recipient) => {
       return (
         <div style={cardStyles}>
+          <Typography style={{margin: 0, padding: '10px 0 0 15px', alignSelf: 'flex-start', color: '#bbb'}} variant="subtitle1">Recipient</Typography>
           <Recipient
             changeRecipients={this.changeRecipients}
             recipient={recipient}
